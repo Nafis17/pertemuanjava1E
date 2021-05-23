d0d994661a5cf0c87d8fc066801acf9ffb4dac31
Nafis17 <sabitgt17@gmail.com>
2021-05-23 10:56:28 +0800

    Initial commit

---
+++ src/id/ac/uniska/DataType.java
@@ -0,0 +1,29 @@
+package id.ac.uniska;
+
+import javax.swing.*;
+
+public class DataType {
+
+    public static void main(String[] args){
+
+        //static datatype
+
+        String nama         = "Muhammadf Nafis";
+        int usia            = 20;
+        int target_kuliah   = 4;
+        double ipk          = 3.9;
+        char nilai          = 'A';
+        boolean tampan      = true;
+
+        System.out.println("Nama            = " + nama);
+        System.out.println("Usia            = " + usia);
+        System.out.println("Target Kuliah   = " + target_kuliah);
+        System.out.println("IPK             = " + ipk);
+        System.out.println("Nilai PBO       = " + nilai);
+        System.out.println("Tampan          = " + tampan);
+
+        JOptionPane.showMessageDialog(
+               
+
+    }
+}
