38e1c15a9279d8a84388b696041aed04abe6b125
Nafis17 <sabitgt17@gmail.com>
2021-05-23 11:38:25 +0800

    String

---
+++ src/id/ac/uniska/StringJava.java
@@ -0,0 +1,9 @@
+package id.ac.uniska;
+
+public class StringJava {
+    public static void main(String[] args){
+        char[] uniskaChar ={'U','N','I','S','K','A'};
+        String uniskaString = new String(uniskaChar);
+        System.out.println(uniskaString);
+    }
+}
